7695e49341fa876d497cee1b88b9c7719833b0d2
Marcus Will <marcusxwill@gmail.com>
2024-01-25 14:45:47 +0100

    add grid and breakpoints

---
+++ src/components/widget/Widget.tsx
@@ -0,0 +1,11 @@
+import './Widget.scss'
+
+function Widget() {
+    return (
+        <div className='widget'>
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dolorum facilis rerum, expedita repellat illo odio. Id ratione expedita explicabo fuga, debitis nobis suscipit fugiat modi vero sit eaque ea.
+        </div>
+    )
+}
+
+export default Widget
--- src/templates/dashboard/Dashboard.tsx
+++ src/templates/dashboard/Dashboard.tsx
@@ -1,11 +1,29 @@
 import Menu from "../../components/menu/Menu"
+import Widget from "../../components/widget/Widget"
+import './Dashboard.scss'
 
 function Dashboard() {
-  return (
-    <div>
-      <Menu></Menu>
-    </div>
-  )
+    return (
+        <div className="dashboard">
+            <div className="dashboard__menu">
+                <Menu></Menu>
+            </div>
+            <div className="dashboard__header">
+                <h2>Welcome back, Friend</h2>
+            </div>
+            <div className="dashboard__grid">
+                <div className="dashboard__grid__widget1">
+                    <Widget></Widget>
+                </div>
+                <div className="dashboard__grid__widget2">
+                    <Widget></Widget>
+                </div>
+                <div className="dashboard__grid__widget3">
+                    <Widget></Widget>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Dashboard
