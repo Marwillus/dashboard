dedc2d1297eebd2e45638af4cadf62a0de2147d3
Marcus Will <marcusxwill@gmail.com>
2024-01-28 16:35:19 +0100

    add animated modal

--- src/components/widget/Widget.tsx
+++ src/components/widget/Widget.tsx
@@ -1,5 +1,6 @@
-import { ArrowRightUp } from '@icon-park/react'
+import { ArrowRightUp, Close, ReadBook } from '@icon-park/react'
 import './Widget.scss'
+import { useEffect, useRef } from 'react'
 
 interface WidgetProps {
     author: string | null
@@ -11,6 +12,30 @@ interface WidgetProps {
 }
 
 function Widget({ author, title, description, url, urlToImage, content }: WidgetProps) {
+    const modal = useRef<HTMLDialogElement>(null)
+
+    const openModal = () => {
+        if (!modal.current) return
+        modal.current.showModal();
+    }
+
+    const closeModal = () => {
+        if (!modal.current) return
+
+        modal.current.setAttribute("closing", "");
+
+        modal.current.addEventListener(
+            "animationend",
+            () => {
+                if (!modal.current) return
+
+                modal.current.removeAttribute("closing");
+                modal.current.close();
+            },
+            { once: true }
+        );
+    }
+
     return (
         <div className='widget'>
             {urlToImage && <div className="widget__image">
@@ -18,9 +43,18 @@ function Widget({ author, title, description, url, urlToImage, content }: Widget
             </div>}
             <h4 className='widget__header'>{title}</h4>
             {description && <p className='widget__description'>{description}</p>}
-            {url && <button className='widget__button'>
-                <a href={url} rel="noopener noreferrer" target='_blank'>Go To Article<ArrowRightUp /></a>
-            </button>}
+            <div className="widget__button-group">
+                {content && <button className="widget__button" onClick={openModal}>Read more <ReadBook /></button>}
+                {url && <button className='widget__button'>
+                    <a href={url} rel="noopener noreferrer" target='_blank'>Go to article<ArrowRightUp /></a>
+                </button>}
+            </div>
+            {content && <dialog ref={modal} className='modal'>
+                <button className='close' onClick={closeModal}><Close /></button>
+                <h3>{title}</h3>
+                <p>{content}</p>
+            </dialog>
+            }
         </div>
     )
 }
